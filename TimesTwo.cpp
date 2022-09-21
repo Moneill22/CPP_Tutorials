f34e04d8cc1a37936832c743491efcfd0dbf4ea7
Matt <mattoneill215@gmail.com>
2022-09-20 21:54:18 -0400

    Work with input

---
+++ TimesTwo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main(){
+
+	std::cout << "Input a number and I will double it back to you: ";
+
+	int x;
+	std::cin >> x;
+	
+	std::cout <<2*x << " is twice your number!";
+
+	return 0;
+}
